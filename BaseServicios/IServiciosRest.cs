bcf6874b2db13a773db9fc6d17670702fc42b26a
Felix <martinez.alvaro.felix@gmail.com>
2015-11-26 14:27:22 +0100

    Primera subida

---
+++ BaseServicios/IServiciosRest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseServicios
+{
+    public interface IServiciosRest<TModelo>
+    {
+        //Sincrono: Pido algo y estoy esperando a que llegue el resultado
+        //Asincrono: Yo pido algo se queda haciendolo puedo hacer cosas mientras y cuando ya tiene los resultados los manda
+        //Task : Tarea asincrona
+        //Un task vacio no tiene resultado, es un void si se quieren obtener los detalles agregamos <Tmodelo>
+
+        Task<TModelo> Add(TModelo model);
+        Task Update(TModelo model);
+        Task Delete(TModelo model);
+
+        //Las llamadas a Get (operaciones de consulta) pueden ser sincronas por eso no usamos Task
+        List<TModelo> Get();
+        List<TModelo> Get(Dictionary<String, String> param);
+        TModelo Get(int id);
+    }
+}
---
+++ BaseServicios/ServicioRestImpl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseServicios
+{
+    public class ServicioRestImpl<TModelo>:IServiciosRest<TModelo>
+    {
+        //La url donde esta el servicio
+        private String url;
+        //Auth es para saber si el servicio es autenticado o no
+        private bool auth;
+        //User es para, si el servicio es autenticado, saber cual es el nombre de usuario
+        private String user;
+        //Pass es para, si el servicio es autenticado, saber cual es el password de usuario
+        private String pass;
+                
+        public ServicioRestImpl(String url, bool auth = false,String user = null,String pass = null)
+        {
+            this.url = url;
+            this.auth = auth;
+            this.user = user;
+            this.pass = pass;
+        } 
+
+        public Task<TModelo> Add(TModelo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TModelo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TModelo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TModelo> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TModelo> Get(Dictionary<string, string> param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TModelo Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
